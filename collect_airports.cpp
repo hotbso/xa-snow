83883314f13ec4c5bfe51d1d49a29640a3d7bfe4
Holger Teutsch <holger.a.teutsch@gmail.com>
2026-01-08 19:37:06 +0100

    reformat in Google style

--- collect_airports.cpp
+++ collect_airports.cpp
@@ -27,8 +27,7 @@
 std::vector<std::unique_ptr<Airport>> airports;
 
 // SceneryPacks constructor
-SceneryPacks::SceneryPacks(const std::string& xp_dir)
-{
+SceneryPacks::SceneryPacks(const std::string& xp_dir) {
     std::string scpi_name(xp_dir + "/Custom Scenery/scenery_packs.ini");
 
     std::ifstream scpi(scpi_name);
@@ -73,20 +72,16 @@ SceneryPacks::SceneryPacks(const std::string& xp_dir)
     sc_paths.shrink_to_fit();
 }
 
-static void
-SplitWords(std::string str, std::vector<std::string>& words)
-{
-	char *pch = strtok((char *)str.c_str(), " ");
-	while (pch != NULL) {
-		words.push_back(pch);
-		pch = strtok (NULL, " ");
-	}
+static void SplitWords(std::string str, std::vector<std::string>& words) {
+    char* pch = strtok((char*)str.c_str(), " ");
+    while (pch != NULL) {
+        words.push_back(pch);
+        pch = strtok(NULL, " ");
+    }
 }
 
 // go through apt.dat and collect runways from 100 lines
-static bool
-ParseAptDat(const std::string& fn, Airport& arpt)
-{
+static bool ParseAptDat(const std::string& fn, Airport& arpt) {
     std::ifstream apt(fn);
     if (apt.fail())
         return false;
@@ -94,43 +89,44 @@ ParseAptDat(const std::string& fn, Airport& arpt)
     LogMsg("Processing '%s'", fn.c_str());
 
     std::string line;
-    line.reserve(2000);          // can be quite long
+    line.reserve(2000);  // can be quite long
 
     while (std::getline(apt, line)) {
         size_t i = line.find('\r');
         if (i != std::string::npos)
             line.resize(i);
 
-		//1    681 0 0 ENGM Oslo Gardermoen
+        // 1    681 0 0 ENGM Oslo Gardermoen
         if (line.find("1 ") == 0) {
-			//LogMsg("%s", line.c_str());
-			int ofs;
-			sscanf(line.c_str(), "%*d %*d %*d %*d %n", &ofs);
-			if (ofs < (int)line.size())
-				line.erase(0, ofs);
-			arpt.name = line;
-			continue;
-		}
+            // LogMsg("%s", line.c_str());
+            int ofs;
+            sscanf(line.c_str(), "%*d %*d %*d %*d %n", &ofs);
+            if (ofs < (int)line.size())
+                line.erase(0, ofs);
+            arpt.name = line;
+            continue;
+        }
 
-		//100 45.11 15 0 0.00 1 3 0 01L  60.18499584  011.07373840 0 148 3 1 0 0 19R  60.21615335  011.09170422 0 140 3 2 1 0
+        // 100 45.11 15 0 0.00 1 3 0 01L  60.18499584  011.07373840 0 148 3 1 0 0 19R  60.21615335  011.09170422 0 140 3
+        // 2 1 0
         if (line.find("100 ") == 0) {
-			std::vector<std::string> words;
-			SplitWords(line, words);
+            std::vector<std::string> words;
+            SplitWords(line, words);
 
-			int code = std::atoi(words[2].c_str());
-			code %= 100;
-			if (code != 15)
-				continue;
+            int code = std::atoi(words[2].c_str());
+            code %= 100;
+            if (code != 15)
+                continue;
 
-            //LogMsg("%s", line.c_str());
-			Runway rwy;
-			rwy.name = words[8];
-			rwy.end1.lat = std::atof(words[9].c_str());
-			rwy.end1.lon = std::atof(words[10].c_str());
-			rwy.end2.lat = std::atof(words[18].c_str());
-			rwy.end2.lon = std::atof(words[19].c_str());
-			arpt.runways.push_back(rwy);
-		}
+            // LogMsg("%s", line.c_str());
+            Runway rwy;
+            rwy.name = words[8];
+            rwy.end1.lat = std::atof(words[9].c_str());
+            rwy.end1.lon = std::atof(words[10].c_str());
+            rwy.end2.lat = std::atof(words[18].c_str());
+            rwy.end2.lon = std::atof(words[19].c_str());
+            arpt.runways.push_back(rwy);
+        }
     }
 
     apt.close();
@@ -138,63 +134,55 @@ ParseAptDat(const std::string& fn, Airport& arpt)
 }
 
 struct Circle {
-	Vec2 c;
-	float r;
+    Vec2 c;
+    float r;
 };
 
 // circle from 2 points
-static Circle
-CircleFrom(const Vec2& a, const Vec2& b)
-{
-	return { 0.5f * (a + b), 0.5f * len(a - b)};
+static Circle CircleFrom(const Vec2& a, const Vec2& b) {
+    return {0.5f * (a + b), 0.5f * len(a - b)};
 }
-
 
 // circle from 3 points
-static Circle
-CircleFrom(const Vec2& v1, const Vec2& v2, const Vec2& v3)
-{
-	// Check whether a circle around 2 points covers the third one.
-	// It's cheap and covers the case that all 3 are collinear
-	Circle c = CircleFrom(v1, v2);
-	if (len(v3 - c.c) <= c.r)
-		return c;
+static Circle CircleFrom(const Vec2& v1, const Vec2& v2, const Vec2& v3) {
+    // Check whether a circle around 2 points covers the third one.
+    // It's cheap and covers the case that all 3 are collinear
+    Circle c = CircleFrom(v1, v2);
+    if (len(v3 - c.c) <= c.r)
+        return c;
 
-	c = CircleFrom(v1, v3);
-	if (len(v2 - c.c) <= c.r)
-		return c;
+    c = CircleFrom(v1, v3);
+    if (len(v2 - c.c) <= c.r)
+        return c;
 
-	c = CircleFrom(v2, v3);
-	if (len(v1 - c.c) <= c.r)
-		return c;
+    c = CircleFrom(v2, v3);
+    if (len(v1 - c.c) <= c.r)
+        return c;
 
-	Vec2 v21 = v2 - v1;
-	Vec2 v31 = v3 - v1;
+    Vec2 v21 = v2 - v1;
+    Vec2 v31 = v3 - v1;
 
-	float lv1 = v1.x * v1.x + v1.y * v1.y;
-	float lv2 = v2.x * v2.x + v2.y * v2.y;
-	float lv3 = v3.x * v3.x + v3.y * v3.y;
+    float lv1 = v1.x * v1.x + v1.y * v1.y;
+    float lv2 = v2.x * v2.x + v2.y * v2.y;
+    float lv3 = v3.x * v3.x + v3.y * v3.y;
 
-	Vec2 d{ 0.5f * (lv2 - lv1), 0.5f * (lv3 - lv1)};
-	float D = v21.x * v31.y - v31.x * v21.y;
-	Vec2 cc{(d.x * v31.y - d.y * v21.y) / D,
-			(v21.x * d.y - v31.x * d.x) / D};
-	return {cc, len(v1 - cc)};
+    Vec2 d{0.5f * (lv2 - lv1), 0.5f * (lv3 - lv1)};
+    float D = v21.x * v31.y - v31.x * v21.y;
+    Vec2 cc{(d.x * v31.y - d.y * v21.y) / D, (v21.x * d.y - v31.x * d.x) / D};
+    return {cc, len(v1 - cc)};
 }
 
-static Circle
-min_circle_trivial(std::vector<Vec2>& P)
-{
-	if (P.empty())
-		return { { 0, 0 }, 0 };
+static Circle min_circle_trivial(std::vector<Vec2>& P) {
+    if (P.empty())
+        return {{0, 0}, 0};
 
-	if (P.size() == 1)
-		return { P[0], 0 };
+    if (P.size() == 1)
+        return {P[0], 0};
 
-	if (P.size() == 2)
-		return CircleFrom(P[0], P[1]);
+    if (P.size() == 2)
+        return CircleFrom(P[0], P[1]);
 
-	return CircleFrom(P[0], P[1], P[2]);
+    return CircleFrom(P[0], P[1], P[2]);
 }
 
 // Welzl's algorithm for the MEC
@@ -202,37 +190,33 @@ min_circle_trivial(std::vector<Vec2>& P)
 // P = set of points
 // R = set of boundary points
 // n = # of points in P
-static Circle
-Welzl(std::vector<Vec2>& P, std::vector<Vec2> R, int n)
-{
-	// Base case when all points processed or |R| = 3
-	if (n == 0 || R.size() == 3) {
-		return min_circle_trivial(R);
-	}
+static Circle Welzl(std::vector<Vec2>& P, std::vector<Vec2> R, int n) {
+    // Base case when all points processed or |R| = 3
+    if (n == 0 || R.size() == 3) {
+        return min_circle_trivial(R);
+    }
 
-	// Pick a random point randomly
-	int idx = rand() % n;
-	Vec2 p = P[idx];
+    // Pick a random point randomly
+    int idx = rand() % n;
+    Vec2 p = P[idx];
 
-	// Put the picked point at the end of P
-	// since it's more efficient than
-	// deleting from the middle of the vector
-	std::swap(P[idx], P[n - 1]);
+    // Put the picked point at the end of P
+    // since it's more efficient than
+    // deleting from the middle of the vector
+    std::swap(P[idx], P[n - 1]);
 
-	Circle d = Welzl(P, R, n - 1);
-	if (len(d.c - p) <= d.r)
-		return d;
+    Circle d = Welzl(P, R, n - 1);
+    if (len(d.c - p) <= d.r)
+        return d;
 
-	// Otherwise, must be on the boundary of the MEC
-	R.push_back(p);
+    // Otherwise, must be on the boundary of the MEC
+    R.push_back(p);
 
-	// Return the MEC for P - {p} and R U {p}
-	return Welzl(P, R, n - 1);
+    // Return the MEC for P - {p} and R U {p}
+    return Welzl(P, R, n - 1);
 }
 
-bool
-CollectAirports(const std::string& xp_dir)
-{
+bool CollectAirports(const std::string& xp_dir) {
     SceneryPacks scp(xp_dir);
     if (scp.sc_paths.size() == 0) {
         LogMsg("Can't collect scenery_packs.ini");
@@ -241,24 +225,24 @@ CollectAirports(const std::string& xp_dir)
 
     airports.reserve(50);
 
-	for (auto & path : scp.sc_paths) {
+    for (auto& path : scp.sc_paths) {
         airports.emplace_back(std::make_unique<Airport>());
-        Airport &arpt = *airports.back();
+        Airport& arpt = *airports.back();
         ParseAptDat(path + "Earth nav data/apt.dat", arpt);
         if (arpt.runways.size() == 0)
             airports.pop_back();
         else
             arpt.runways.shrink_to_fit();
-	}
+    }
 
-    for (auto & arpt : airports) {
+    for (auto& arpt : airports) {
         LogMsg("%s", arpt->name.c_str());
         std::vector<Vec2> rwy_ends;
 
-        LLPos base = arpt->runways[0].end1;	// pick arbitrary base for circle computation
-        for (auto & rw : arpt->runways) {
-            LogMsg("  rw: %-3s, end1: (%0.4f, %0.4f), end2: (%0.4f, %0.4f)",
-                    rw.name.c_str(), rw.end1.lat, rw.end1.lon, rw.end2.lat, rw.end2.lon);
+        LLPos base = arpt->runways[0].end1;  // pick arbitrary base for circle computation
+        for (auto& rw : arpt->runways) {
+            LogMsg("  rw: %-3s, end1: (%0.4f, %0.4f), end2: (%0.4f, %0.4f)", rw.name.c_str(), rw.end1.lat, rw.end1.lon,
+                   rw.end2.lat, rw.end2.lon);
             rwy_ends.push_back(rw.end1 - base);
             rwy_ends.push_back(rw.end2 - base);
         }
@@ -271,8 +255,7 @@ CollectAirports(const std::string& xp_dir)
 
         arpt->mec_center = base + c.c;
         arpt->mec_radius = c.r;
-        LogMsg("Center: (%0.4f, %0.4f), r = %0.1f",
-                arpt->mec_center.lat, arpt->mec_center.lon, arpt->mec_radius);
+        LogMsg("Center: (%0.4f, %0.4f), r = %0.1f", arpt->mec_center.lat, arpt->mec_center.lon, arpt->mec_radius);
     }
 
     airports.shrink_to_fit();
@@ -280,10 +263,9 @@ CollectAirports(const std::string& xp_dir)
 }
 
 #ifdef TEST_AIRPORTS
-int main()
-{
-	bool res = CollectAirports("e:/X-Plane-12-test");
-	LogMsg("res: %d", res);
-	return 0;
+int main() {
+    bool res = CollectAirports("e:/X-Plane-12-test");
+    LogMsg("res: %d", res);
+    return 0;
 }
 #endif
